5604b1526e8daabb4188248dc08d9ff282c1d835
hosseinjafar <hosseinj.2555@gmail.com>
2024-04-03 13:18:59 -0400

    removed build files

--- GDCM-build/Utilities/VTK/CMakeFiles/vtkgdcm-hierarchy.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "../../lib/vtk-9.3/hierarchy/vtkgdcm/vtkgdcm-hierarchy.txt"
-  "CMakeFiles/vtkgdcm-hierarchy"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/vtkgdcm-hierarchy.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- GDCM-build/Utilities/VTK/CMakeFiles/vtkgdcm-hierarchy.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty custom commands generated dependencies file for vtkgdcm-hierarchy.
-# This may be replaced when dependencies are built.
